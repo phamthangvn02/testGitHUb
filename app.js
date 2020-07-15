a5efa00d51f4b73c14902337aab10b446884793d
Phạm Ngọc Thắng <king0mous2@gmail.com>
2020-07-15 13:14:47 +0700

    update

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log('Hello Node.js');
+var fun={
+tongN: function tongN(n)
+{
+    let s=0;
+    for(i=1;i<=n;i++)
+    {
+        s+=i;
+
+    }
+    return s;
+},
+giaiThua: function giaithua(s)
+{
+    let n=1;
+    for (i=1;i<=s;i++)
+    {
+        n=n*i;
+    }
+    return n;
+},
+
+tongphanSo: function tongphanso(n)
+{
+    let a=1;
+    for(i=2;i<=n;i++)
+    {
+        a=a+(1/i);
+    }
+    return a;
+}
+}
+module.exports=fun;
